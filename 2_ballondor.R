ef497fa80e1d61fd61d9eb797f98158d8e4564ef
Soyoung Jang <76713906+so02e@users.noreply.github.com>
2021-03-17 18:32:40 +0900

    19발롱도르 스코어
    데이터셋 가져오기

--- 2_ballondor.R
+++ 2_ballondor.R
@@ -1,5 +1,4 @@
 # 2. 2019년도 발롱도르 순위와 선수 능력치의 상관관계
-# 프랑스어 선수명
 library(dplyr)
 library(stringr)
 library(RSelenium)
@@ -7,19 +6,21 @@ library(RSelenium)
 remDr <- remoteDriver(remoteServerAddr = "localhost" , port = 4445, browserName = "chrome")
 remDr$open()
 
-url <- "https://www.goal.com/en-za/lists/ballon-dor-2019-messi-ronaldo-van-dijk-set-to-head-up-list-of-30-/1ac0k3mdohx5y1rjinc2y98lv3"
+url <- "https://en.wikipedia.org/wiki/2019_Ballon_d%27Or"
 remDr$navigate(url)
-ballondordom <- remDr$findElements(using ="css selector","div.widget-slide-list__content > h2")
-ballondor<-sapply(ballondordom,function(x){x$getElementText()})
-is.list(ballondor)
-ballondorlist <- data.frame(unlist(ballondor))
 
-ballondorlist <- data.frame(do.call('rbind',strsplit(as.character(ballondorlist$unlist.ballondor.),split="|",fixed=TRUE)))
+library(XML)
+elem <- remDr$findElement(using="css", value="#mw-content-text > div.mw-parser-output > table:nth-child(7)")
+elemtxt <- elem$getElementAttribute("outerHTML")
+elem_html <- htmlTreeParse(elemtxt, asText = TRUE, useInternalNodes = T, encoding="UTF-8")
 
-ballondorlist <- ballondorlist %>% rename(player=X1, team = X2, nation=X3)
-ballondorlist$player <- str_sub(ballondorlist$player,start = 4) 
-ballondorlist <- ballondorlist %>% mutate(ranking = c(1:30))
-# fifa21 데이터에서 long_name 과 일치하는 것을 뽑아와야 함. 
-View(fifa21)
-View(ballondorlist)
-ballondorname <- as.vector(unlist(ballondorlist %>% select(player)))
+Sys.setlocale("LC_ALL", "English") # 선수 이름 프랑스어 때문에 English로케일로
+ballond19 <- readHTMLTable(elem_html, header = T, stringsAsFactors = FALSE)[[1]]
+ballond19 <- ballond19 %>% rename("Rank"="Rank\n","Player"="Player\n","Club(s)"="Club(s)\n","Points"="Points\n")
+
+##21행, 25행, 27행, 29행, 30행의 경우에는 공동 순위 때문에 한칸씩 밀려서 해결
+naballond19 <- ballond19 %>% filter(is.na(Points)) %>% rename("Player"="Rank","Club(s)"="Player","Points"="Club(s)","Rank"="Points")
+
+ballond19 <- ballond19 %>% subset(!(is.na(Points))) %>% bind_rows(naballond19)  %>% select(2:4) %>% mutate_at(vars(Points),as.numeric) %>% arrange(desc(Points))
+
+
--- dataload.R
+++ dataload.R
@@ -3,3 +3,5 @@ Sys.setlocale("LC_ALL", "English")
 fifa21 <- read.csv("data/players_21.csv",header=TRUE,encoding="UTF-8")
 Sys.setlocale("LC_ALL","Korean")
 head(fifa21)
+
+## java -Dwebdriver.chrome.driver="chromedriver.exe" -jar selenium-server-standalone-4.0.0-alpha-1.jar -port 4445
