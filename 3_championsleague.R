b7a6c624d09f7cbd149d72c9f97c251b35fed222
Soyoung Jang <76713906+so02e@users.noreply.github.com>
2021-03-17 20:38:34 +0900

    데이터 전처리

--- 3_championsleague.R
+++ 3_championsleague.R
@@ -1,4 +1,16 @@
 # 3. 챔피언스 리그 배당률과 게임에서의 급료와의 상관관계
+library(dplyr)
+library(stringr)
+library(RSelenium)
+remDr <- remoteDriver(remoteServerAddr = "localhost" , port = 4445, browserName = "chrome")
+remDr$open()
+url <- "https://www.bet365.com/#/AC/B1/C172/D1/E51300756/F2" # 안 들어가짐. ?
+remDr$navigate(url)
+## 셀레늄 쓰지 말고 들어가보기
 
-# https://www.bet365.com/#/AC/B1/C172/D1/E51300756/F2/
 
+
+championsteam <-c("Manchester City", "FC Bayern München", "Paris Saint-Germain", "Liverpool", "Real Madrid", "Chelsea", "Borussia Dortmund", "Atlético Madrid", "FC Porto", "Lazio")
+
+allwage <- fifa21  %>% group_by(club_name)%>% summarise(sumwage=mean(wage_eur))
+View(allwage)
--- 4_league.R
+++ 4_league.R
@@ -1,4 +1,17 @@
 # 4. 리그 점수와 선수 등급과의 상관관계
-# https://www.uefa.com/memberassociations/uefarankings/country/#/yr/2021
+library(dplyr)
+library(stringr)
+library(RSelenium)
+
+remDr <- remoteDriver(remoteServerAddr = "localhost" , port = 4445, browserName = "chrome")
+remDr$open()
+url <- "https://www.uefa.com/memberassociations/uefarankings/country/#/yr/2021"
+remDr$navigate(url)
 
 
+library(XML)
+elem <- remDr$findElement(using="css", value="#DataTables_Table_0 > tbody > tr")
+elemtxt <- elem$getElementAttribute("outerHTML")
+elem_html <- htmlTreeParse(elemtxt, asText = TRUE, useInternalNodes = T, encoding="UTF-8")
+
+uefascore <- readHTMLTable(elem_html, header = T, stringsAsFactors = FALSE)[[1]]
